9ca9c778b3ebe4d8b65b9378daf204d039de2994
amabel1234 <lolipopajakok12@gmail.com>
2026-06-22 18:28:34 +0700

    feat: admin panel + premium popup + chat limit

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -3,3 +3,6 @@ export * from "./messages";
 export * from "./users";
 export * from "./payments";
 export * from "./message_counts";
+export * from "./pricing";
+export * from "./payment_methods";
+export * from "./site_settings";
